f38fd1bbdd2510aff453005808c789981552071e
dannbongo <danieleaton15@hotmail.co.uk>
2023-03-04 09:47:00 +0000

    Started position outputs relative to image - sig touch

--- script.js
+++ script.js
@@ -14,12 +14,7 @@ buttons.forEach(button => {
   });
 });
 
-//needed to initialise this button here because we need this variable in 2 functions
-let caseMaterialSelect = document.getElementById("caseMaterial");
 
-
-
-//Signature touch calculations
 let calculateBtn = document.getElementById("calculateBtn");
 calculateBtn.addEventListener("click", calculate);
 
@@ -54,26 +49,23 @@ function signatureTouchDimACalc(inputApetureWidth,inputApetureHeight){
 
   if (inputApetureHeight === 50 && inputApetureWidth >= 100 && inputApetureWidth <= 1000){
     dimensionA = 137.5
-    document.getElementById("dimensionA").value = dimensionA;
   } else if (inputApetureHeight >= 75 && inputApetureWidth >= 100 && inputApetureWidth <= 250){
     dimensionA = 125
-    document.getElementById("dimensionA").value = dimensionA;
   } else if (inputApetureHeight >= 75 && inputApetureWidth >= 275 && inputApetureWidth <= 1000){
     dimensionA = 135
-    document.getElementById("dimensionA").value = dimensionA;
   } else if (inputApetureHeight >= 50 && inputApetureWidth >= 1025 && inputApetureWidth <= 1500){
     dimensionA = 145
-    document.getElementById("dimensionA").value = dimensionA;
   } else if (inputApetureHeight >= 50 && inputApetureWidth >= 1525 && inputApetureWidth <= 2000){
     dimensionA = 155
-    document.getElementById("dimensionA").value = dimensionA;
   } else if (inputApetureHeight >= 50 && inputApetureWidth >= 2025 && inputApetureWidth <= 2500){
     dimensionA = 165
-    document.getElementById("dimensionA").value = dimensionA;
   } else if (inputApetureHeight >= 50 && inputApetureWidth >= 2525 && inputApetureWidth <= 3000){
     dimensionA = 175
-    document.getElementById("dimensionA").value = dimensionA;
-}}
+  }
+  document.getElementById("dimensionA1").value = dimensionA
+  document.getElementById("dimensionA2").value = dimensionA
+  document.getElementById("dimensionA3").value = dimensionA
+  }
 
 
 function signatureTouchDimZCalc(apetureMin, caseMaterialSelect){
@@ -132,28 +124,16 @@ function signatureTouchDimMFZCalc(apetureMin){
 
 
 /*
-ChatGPT Comments
+lessons learnt
 
-In this example, we've created three buttons with a common class name of new-window-button, and we've added a data-url attribute to each button to specify the URL of the page to be loaded. We've also created a reusable openNewWindow() function that takes a URL parameter and uses the window.open() method to load the specified page.
+It clicked nesting functions within functions and using variables within the nested functions by initialising them in the top level function and passing them down to the lower functions
 
-Finally, we've used JavaScript to add a click event listener to each button. When a button is clicked, the listener calls the openNewWindow() function with the URL specified in the data-url attribute of the clicked button.
 
-This approach allows you to add as many buttons and windows as you like, and makes it easy to update the functionality in one place if you need to make changes to the way windows are opened or managed.
 
-My Comments
 
-We might need to variable 'buttons' from node list to an array
 
-For each element in our array of buttons, apply the function called button.
-to each button, add an event listener. When clicked run the anonymous function. 
-this. refers to object that is called the function, in this case our button. getAttribute is a method that returns the value of class. In our case this is the url assigned to the button
-becuase 'url; is a variable, we can pass this as a argument through out function
+Improvements
 
-the openNewWindow function simply opens out new URl into a new window.
-
-The reason why the webpage is resetting when the button is clicked is because the default behavior of a button inside a form is to submit the form when it is clicked. In this case, the button has the type="button" attribute, which means it is not meant to submit the form.
-
-However, since the button is inside a <form> tag and does not have the type="button" attribute, the default behavior of the button is to submit the form when it is clicked. This causes the page to reload and reset the form.
-
-SOLUTION: SET CALCULATE BUTTON TYPE TO BUTTON IN THE HTML
+Dont use buttons for naviagtion, only for performing actions
+Review the HTML, should only be using 1 H1 tag per page - Review semantic HTML practices
 */
