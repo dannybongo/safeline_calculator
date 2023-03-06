896b58c160ad914f1d1fb020de28d6923a0a2062
dannbongo <danieleaton15@hotmail.co.uk>
2023-03-06 15:21:07 +0000

    Wrote sig touch RZ function for calculating Z and ZcT

--- script.js
+++ script.js
@@ -19,31 +19,107 @@ let calculateBtn = document.getElementById("calculateBtn");
 calculateBtn.addEventListener("click", calculate);
 
 function calculate() {
-
   let inputApetureWidth = parseInt(document.getElementById("inputApetureWidth").value);
   let inputApetureHeight = parseInt(document.getElementById("inputApetureHeight").value);
   let apetureMin = Math.min(inputApetureWidth, inputApetureHeight);
   let caseMaterialSelect = document.getElementById("caseMaterial");
-  let caseDutyType = document.getElementById('caseDutyType');
-  let detectorTypeSelect = document.getElementById('detectorType');
+  let caseDutyType = document.getElementById("caseDutyType");
+  let detectorTypeSelect = document.getElementById("detectorType");
+
+  let array = [];
+  for (let i = 50; i <= 1000; i += 25) {
+    array.push(i)
 
   document.getElementById("outputApetureWidth").value = inputApetureWidth;
   document.getElementById("outputApetureHeight").value = inputApetureHeight;
 
- 
-    if (detectorTypeSelect.value === 'signatureTouch') {
-      signatureTouchDimACalc(inputApetureWidth,inputApetureHeight);
-      signatureTouchDimZCalc(apetureMin, caseMaterialSelect);
-      signatureTouchDimZcTCalc(apetureMin, caseMaterialSelect);
-      signatureTouchDimWcTCalc(inputApetureWidth);
-      signatureTouchThkCalc(caseDutyType, caseMaterialSelect);
-      signatureTouchDimMFZCalc(inputApetureWidth,inputApetureHeight);
-    }
-    else {(detectorTypeSelect.value === 'rzSignatureTouch');{
-      console.log('rzSignatureTouch');
-    }}
-  };
+  //The Sig-Touch & Sig-Touch RZ share the same WcT and flange thickness dimensions
+  signatureTouchDimWcTCalc(inputApetureWidth);
+  signatureTouchThkCalc(caseDutyType, caseMaterialSelect);
 
+  if (detectorTypeSelect.value === "signatureTouch") {
+    signatureTouchCalc(inputApetureWidth, inputApetureHeight, apetureMin, caseMaterialSelect)
+  } else {
+    signatureTouchRZCalc(inputApetureWidth, inputApetureHeight, apetureMin, caseMaterialSelect)
+  }
+}
+}
+
+function signatureTouchCalc(inputApetureWidth, inputApetureHeight, apetureMin, caseMaterialSelect){
+  signatureTouchDimACalc(inputApetureWidth,inputApetureHeight);
+  signatureTouchDimZCalc(apetureMin, caseMaterialSelect);
+  signatureTouchDimZcTCalc(apetureMin, caseMaterialSelect);
+  signatureTouchDimMFZCalc(inputApetureWidth,inputApetureHeight);
+}
+
+function signatureTouchRZCalc(inputApetureWidth, inputApetureHeight, apetureMin, caseMaterialSelect){
+
+  // signatureTouchRZ_DimA_SS_Calc()
+  // signatureTouchRZ_DimMFZ_SS_Calc()
+  // signatureTouchRZ_DimDMFZ_SS_Calc()
+
+  if (caseMaterialSelect.value === "stainlessSteel" ){
+    signatureTouchRZ_DimZ_SS_Calc(inputApetureHeight, inputApetureWidth, array);
+    signatureTouchRZ_DimZcT_SS_Calc(inputApetureHeight, inputApetureWidth, array);
+  } else {
+    signatureTouchRZ_DimZ_PA_Calc(inputApetureHeight, inputApetureWidth, array);
+    signatureTouchRZ_DimZcT_PA_Calc(inputApetureHeight, inputApetureWidth, array);
+  }
+
+}
+
+function signatureTouchRZ_DimZ_SS_Calc(inputApetureHeight, inputApetureWidth, array) {
+
+  let index = array.indexOf(inputApetureHeight); //if the apeture height is in the array, return the index of the value in the array
+  let signatureTouchRZ_DimZ_SS_Oversquare_Array = [275,275,275,275,275,275,275,275,306,338,369,400,432,464,494,525,556,588,619,650,682,713,744,775,807,838,869,900,932,963,994,1025,1057,1088,1119,1150,1182,1213,1244];
+  let signatureTouchRZ_DimZ_SS_Rect_Array = [275,275,275,275,275,319,354,387,419,434,444,457,469,481,494,525,556,588,619,650,682,713,744,775,807,838,869,900,932,963,994,1025,1057,1088,1119,1150,1182,1213,1244];
+  
+  if (inputApetureHeight >= inputApetureWidth){
+    document.getElementById("dimensionZ").value = signatureTouchRZ_DimZ_SS_Oversquare_Array[index]
+  } else {
+    document.getElementById("dimensionZ").value =signatureTouchRZ_DimZ_SS_Rect_Array[index]
+  }
+ }
+
+function signatureTouchRZ_DimZcT_SS_Calc(inputApetureHeight, inputApetureWidth, array) {
+
+  let index = array.indexOf(inputApetureHeight);
+  let signatureTouchRZ_DimZcT_SS_Oversquare_Array = [237,237,237,237,237,237,237,237,268,300,331,362,394,426,456,487,518,550,581,612,644,675,706,737,769,800,830,862,894,925,956,987,1019,1050,1081,1112,1144,1175,1206]
+  let signatureTouchRZ_DimZcT_SS_Rect_Array = [237,237,237,237,237,281,316,349,381,396,406,419,431,443,456,487,518,550,581,612,644,675,706,737,769,800,830,862,894,925,956,987,1019,1050,1081,1112,1144,1175,1206]
+
+  if (inputApetureHeight >= inputApetureWidth){
+    document.getElementById("dimensionZcT").value = signatureTouchRZ_DimZcT_SS_Oversquare_Array[index]
+  } else {
+    document.getElementById("dimensionZcT").value = signatureTouchRZ_DimZcT_SS_Rect_Array[index]
+  }
+ }
+
+ function signatureTouchRZ_DimZ_PA_Calc(inputApetureHeight, inputApetureWidth, array) {
+
+  let index = array.indexOf(inputApetureHeight);
+  let signatureTouchRZ_DimZ_PA_Oversquare_Array = [1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111] //Tom to confirm these values
+  let signatureTouchRZ_DimZ_PA_Rect_Array = [275,275,275,275,275,325,360,393,425,440,450,463,475,487,500,531,562,594,625,625,625,625,750,750,750,750,875,875,875,875,1000,1000,1000,1000,1125,1125,1125,1125,1250]
+
+
+  if (inputApetureHeight >= inputApetureWidth){
+    document.getElementById("dimensionZcT").value = signatureTouchRZ_DimZ_PA_Oversquare_Array[index]
+  } else {
+    document.getElementById("dimensionZcT").value = signatureTouchRZ_DimZ_PA_Rect_Array[index]
+  }
+ }
+
+ function signatureTouchRZ_DimZcT_PA_Calc(inputApetureHeight, inputApetureWidth, array) {
+
+  let index = array.indexOf(inputApetureHeight);
+  let signatureTouchRZ_ZctDim_SS_Oversquare_Array = [1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111,1111111] //Tom to confirm these values
+  let signatureTouchRZ_ZctDim_SS_Rect_Array = [237,237,237,237,237,281,316,349,381,396,406,419,431,443,456,487,518,550,581,581,581,581,706,706,706,706,830,830,830,830,956,956,956,956,1081,1081,1081,1081,1206]
+ 
+  if (inputApetureHeight >= inputApetureWidth){
+    document.getElementById("dimensionZcT").value = signatureTouchRZ_ZctDim_SS_Oversquare_Array[index]
+  } else {
+    document.getElementById("dimensionZcT").value = signatureTouchRZ_ZctDim_SS_Rect_Array[index]
+  }
+ }
 
 function signatureTouchDimACalc(inputApetureWidth,inputApetureHeight){
 
@@ -90,19 +166,19 @@ function signatureTouchDimWcTCalc(inputApetureWidth){
 
 function signatureTouchThkCalc(caseDutyType, caseMaterialSelect){
 
-  if (caseDutyType.value === 'heavyDuty' && caseMaterialSelect.value === "paintedAluminium"){
+  if (caseDutyType.value === "heavyDuty" && caseMaterialSelect.value === "paintedAluminium"){
     document.getElementById("dimensionThk_LHS").value = 17
     document.getElementById("dimensionThk_RHS").value = 17
   }
-  if (caseDutyType.value === 'heavyDuty' && caseMaterialSelect.value === "stainlessSteel"){
+  if (caseDutyType.value === "heavyDuty" && caseMaterialSelect.value === "stainlessSteel"){
     document.getElementById("dimensionThk_LHS").value = 17
     document.getElementById("dimensionThk_RHS").value = 17
   }
-  else if (caseDutyType.value === 'mediumDuty' && caseMaterialSelect.value === "paintedAluminium"){
+  else if (caseDutyType.value === "mediumDuty" && caseMaterialSelect.value === "paintedAluminium"){
     document.getElementById("dimensionThk_LHS").value = 4.5
     document.getElementById("dimensionThk_RHS").value = 4.5
   }
-  else if (caseDutyType.value === 'mediumDuty' && caseMaterialSelect.value === "stainlessSteel"){
+  else if (caseDutyType.value === "mediumDuty" && caseMaterialSelect.value === "stainlessSteel"){
     document.getElementById("dimensionThk_LHS").value = 13
     document.getElementById("dimensionThk_RHS").value = 13
   }
